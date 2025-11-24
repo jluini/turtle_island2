a8144cc48a84b277e5392301ffcbe16d0751a75e
jluini <jluini@gmail.com>
2025-11-24 16:08:38 -0300

    Varias cosas

--- spawn_point.gd
+++ spawn_point.gd
@@ -1,3 +1,42 @@
+@tool
+
 extends Marker2D
 
-@export var flipped: bool = false
+@export var player : int = 0:
+	set(new_player):
+		player = new_player
+		queue_redraw()
+
+@export var flipped : bool = false:
+	set(new_flipped):
+		flipped = new_flipped
+		queue_redraw()
+
+const COLORS = [
+	Color.LIGHT_GRAY,
+	Color.BLUE,
+	Color.RED,
+	Color.GREEN,
+	Color.YELLOW,
+	Color.CYAN,
+	Color.MAGENTA,
+	Color.DARK_GRAY,
+	Color.ORANGE
+]
+
+const ERROR_COLOR = Color.BLANCHED_ALMOND
+
+func _draw():
+	if Engine.is_editor_hint():
+		var origin : Vector2 = Vector2.ZERO
+		var delta : Vector2 = Vector2(30.0, 0.0)
+		var factor = -1.0 if flipped else +1.0
+		var color = get_color()
+		
+		draw_line(origin, origin + factor * delta, color, 4.0)
+
+func get_color() -> Color:
+	if player >= 0 and player < COLORS.size():
+		return COLORS[player]
+	else:
+		return ERROR_COLOR
